1097ec09c4fcf8ebbe42255e43d3341f1c69127a
adxzhangofficial <adxzhangofficial@gmail.com>
2026-01-06 11:46:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-})
+  base: '/', // if you use a custom domain root, keep '/' 
+  // if you deploy to a repo subpath, e.g., /my-repo/, set base: '/my-repo/'
+});
